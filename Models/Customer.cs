df06907699db38f831977a7f6744d0671f982eea
Thomas Kibaru <kibtom@yahoo.com>
2017-09-28 18:09:56 +0100

    update vidly

---
+++ Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Customer
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        public bool IsSubscribedToNewsLetter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        [Required]
+        public byte MembershipTypeID { get; set; }
+        public DateTime? Birthdate { get; set; }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+namespace Vidly.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int GenreId { get; set; }
+        public Genre Genre { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int NumberInStock { get; set; }
+
+    }
+}
